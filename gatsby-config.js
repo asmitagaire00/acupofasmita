c84aae19e87bd590843f85bccc68f21c061de3b8
Asmita Gaire <asmitagaire00@gmail.com>
2021-12-28 18:55:17 +1100

    Add new favicon image for browser title.

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#333`,
-        icon: `src/images/my-logo.png` // This path is relative to the root of the site.
+        icon: `src/images/coffee.png` // This path is relative to the root of the site.
       }
     }, // To learn more, visit: https://gatsby.dev/offline // this (optional) plugin enables Progressive Web App + Offline functionality
     `gatsby-plugin-offline`
